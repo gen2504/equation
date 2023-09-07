2f396af50ce56274c4efeafa778f1078c5e820ca
--global <--global>
2023-09-07 21:58:54 +0400

    finished project

--- py.py
+++ py.py
@@ -9,23 +9,39 @@ elif inp == 1:
 elif inp == 2:
     a = int(input('Enter first argument: '))
     b = int(input('Enter second argument: '))
-xpos = int(input('Where do you want x to be(enter pos 1, 2 or 3):\n')
+xpos = int(input('Where do you want x to be(enter pos 1, 2 or 3):\n'))
 
 print('a = ' + str(a) + ', b = ' + str(b))
 char = random.randint(0,1)
 if char == '1':
-        if xpos == '1':
-            x = b-a
-            print('x' + a +'=' + b)
-        if xpos == '2':
-            x = b-a
-            print('a' + 'x' +'=' + b)
-        elif xpos == '3':
-            x = b+a
-            print(a + '+' + b + '=' + 'x')
-             
-    print(str(a) + '+' + str(b) + '=' + str(c))
+    if str(xpos) == '1':
+        x = int(b)-int(a)
+        print('x' +'+'+ str(a) +'=' + str(b))
+        print('x' + '=' + str(b) + '-' + str(a))
+        print('x' + '=' + str(x))
+    elif str(xpos) == '2':
+        x = int(b)-int(a)
+        print(str(a) +'+x' +'=' + str(b))
+        print('x' + '=' + str(b) + '-' + str(a))
+        print('x' + '=' + str(x))
+    elif str(xpos) == '3':
+        x = int(a) + int(b)
+        print(str(a) + '+' + str(b) + '=' + 'x')
+        print('x' + '=' + str(x))
+            
 else:
-    c = a-b
-    print(str(a) + '-' + str(b) + '=' + str(c))
+    if str(xpos) == '1':
+        x = a+b
+        print('x' +'-'+ str(a) +'=' + str(b))
+        print('x' + '=' + str(b) + '+' + str(a))
+        print('x' + '=' + str(x))
+    elif str(xpos)== '2':
+        x = -(int(b)-int(a))
+        print(str(a) +'-x' +'=' + str(b))
+        print('-x' + '=' + str(b) + '-' + str(a))
+        print('x' + '=' + str(x))
+    elif str(xpos)== '3':
+        x = int(a) - int(b)
+        print(str(a) + '-' + str(b) + '=' + 'x')
+        print('x' + '=' + str(x))
 
